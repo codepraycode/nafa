c9b35e7e2b2b77c47411e325707b2b4429c43bfd
Precious Olusola <preciousolusola16@gmail.com>
2024-05-06 16:20:37 +0100

    Added a section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import MV from "@/components/Home/MV";
 import Partners from "@/components/Home/Partners";
 
 export default function Home() {
     return (
         <>
+            <MV />
             <Partners />
         </>
     );
---
+++ src/components/Home/MV.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Section from '../UI/Section'
+import Link from 'next/link'
+
+export default function MV() {
+    return (
+        <Section
+            name='mv'
+        >
+        
+            <p>OUR PRINCIPLES ENCOMPASSES DEMOCRAZY, EQUALITY, INCLUSIVITY, SUSTAINABILITY, INTEGRITY AND EDUCATION.</p>
+            <hr/>
+            <Link href={"/"} className='btn'>
+                Mission & Vision
+            </Link>
+        </Section>
+    )
+}
