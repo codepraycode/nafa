052a35d2abe05d3c845784feebe10c56e5abad27
Precious Olusola <preciousolusola16@gmail.com>
2024-05-06 17:37:35 +0100

    added hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Hero from "@/components/Home/Hero";
 import MV from "@/components/Home/MV";
 import Partners from "@/components/Home/Partners";
 import RG from "@/components/Home/RG";
@@ -5,6 +6,7 @@ import RG from "@/components/Home/RG";
 export default function Home() {
     return (
         <>
+            <Hero />
             <RG />
             <MV />
             <Partners />
---
+++ src/components/Home/Hero.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Section from '../UI/Section'
+import Link from 'next/link'
+
+export default function Hero() {
+    return (
+        <Section
+            name='hero'
+        >
+            <h1>ONE GAME<br/>ONE NATION</h1>
+            <p>Unleash Your Passion: Empowering Your Football Journey Together.</p>
+
+            <div className="cta">
+                <Link href={"/"} className='btn btn-primary'>
+                    Become a Member
+                </Link>
+                <Link href={"/"} className='btn btn-light'>
+                    Enroll Your Organization
+                </Link>
+
+            </div>
+        </Section>
+    )
+}
