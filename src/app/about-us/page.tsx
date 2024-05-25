48589bac6ec2b59ac1161e7d9a6fd444288c8fdb
Precious Olusola <preciousolusola16@gmail.com>
2024-05-25 11:49:04 +0100

    Added Favicon

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -2,6 +2,7 @@ import Partners from "@/components/Home/Partners";
 import PageHeader from "@/components/PageHeader";
 import Section from "@/components/UI/Section";
 import aboutData from "@/data/about.json";
+import Image from "next/image";
 
 
 
@@ -57,6 +58,39 @@ export default function AboutPage() {
                         </ol>
                     </div>
                 </Section>
+
+
+                <br/><br/><br/><br/>
+
+                <Section name="logo_label">
+
+                    <h2 className="section-title">Our Logo</h2>
+                    <br/>
+                    <figure>
+                        <Image
+                            src={"https://s3-alpha-sig.figma.com/img/cf41/4235/19ebcb622408148256d9072033e6052e?Expires=1717372800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=FSWj-MydVPrictzf1AIsJCRjPrdQGSNDcRrl-gCjtfhxqIpOPF7HttPBjNmZbFkArvNK4qmmNc~~XXUDmcklhEcZaDJzoa7MEPXNn~aSVMgcUU6QVvCL7BfiWGe7w4aKcRO0GURmImEOkkLZ31wM1NZcv1fnSEgfTuVM~FzTb2z-jmpA8Iw0EkeAZVIXPO7Cw5Qd-D~Ks2ICxyzLaG7Vc9KQhIDgRa6zfGaeBizc9LZIQW8DdX8IgxmnKBBQUChRaRnBoBHRJtPUfKFBXowY2mCnkWaUbZiz~jfZf1SR8G6QFFUwz4QvzxQKn6w4vjDWJEUCG2WiJ-~8LM4daJoaSg__"}
+                            alt="Logo Meaning"
+                            width={1170}
+                            height={807}
+                        />
+                    </figure>
+
+                </Section>
+                <br/><br/><br/><br/>
+
+                <Section name="objectives">
+                    <ol>
+                        <li>Promote contact and non-contact American Football competitions.</li>
+                        Develop, manage and grow the American Football League and brand within Africa, for international competition (2 years plan).
+                        Develop, manage and grow the American Football League and brand for international competition (5 years plan).
+                        Develop, manage and grow the American Football League in Nigeria.
+                        Provide opportunities for engagement and development to people of all ages, background and abilities within American Football.
+                        The regulation of the game on and off the field of play through oversight of its rules;
+                        The representation of the Nigerian game to national and international partners, be it sporting organizations, federations or commercial interests.
+                    </ol>
+                </Section>
+
+
             </Section>
 
             <Partners/>
