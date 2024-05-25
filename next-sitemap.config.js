9404e83c81b31d1bd0a551d4c7d6278ef1a510e9
Precious Olusola <preciousolusola16@gmail.com>
2024-05-25 18:29:43 +0100

    Added sitemap generation to site

---
+++ next-sitemap.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    siteUrl: 'https://nafa-design.vercel.app', // Replace with your site's URL
+    generateRobotsTxt: true, // (optional) Generate robots.txt file
+    // Additional options:
+    // exclude: ['/page-to-exclude'], // Exclude specific pages or directories
+    // robotsTxtOptions: {    
+    // policies: [
+    //     { userAgent: '*', allow: '/' },
+    //     { userAgent: 'Googlebot', disallow: ['/admin'] },
+    // ],
+    //   additionalSitemaps: [
+    //     'https://www.yourwebsite.com/my-custom-sitemap-1.xml',
+    //     'https://www.yourwebsite.com/my-custom-sitemap-2.xml',
+    //   ],
+    // },
+};
